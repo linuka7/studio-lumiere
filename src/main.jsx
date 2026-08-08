9e517a5a6af764ffe671d4644a1cf368deae989f
linuka7 <linukaipad@gmail.com>
2026-08-08 16:35:54 +0530

    Undo mobile card change and load arrow-only fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
-import "./mobile-fixes.css";
+import "./arrow-fixes.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
